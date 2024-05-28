c34213c63165f5bf4a1c464acf8e2a0022c2ee64
Oscar González Tur <44144257+Krontur@users.noreply.github.com>
2024-05-28 19:31:33 +0200

    Added new quotes and characters

--- apps/backend/scripts/load.quotes.js
+++ apps/backend/scripts/load.quotes.js
@@ -6,7 +6,7 @@ import { deleteMany, insertMany } from '../models/quote.model';
 async function loadQoutes(){
     try {
         // eslint-disable-next-line no-undef
-        const quotes = JSON.parse(readFileSync(join(__dirname, '../data/updated_quotes.json'), 'utf-8'));
+        const quotes = JSON.parse(readFileSync(join(__dirname, '../data/lossimpsonfrases.json'), 'utf-8'));
         await deleteMany({});
         await insertMany(quotes);
         console.log('Quotes loaded successfully. ');
